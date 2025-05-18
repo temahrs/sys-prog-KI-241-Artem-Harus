e44c4121b087acd9b36129791eb415f35b94860b
Artem Mylytsia <amylytsia@gmail.com>
2025-05-19 02:11:44 +0300

    first commit

---
+++ lib/auto.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo $HOSTNAME $HOME >> your-new-file.txt && echo Detected hostname, user >> your-new-file.txt && echo "installing viruses..." >> your-new-file.txt && echo "removing all files from your comp..." >> your-new-file.txt
